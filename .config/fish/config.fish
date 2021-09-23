70eb95607e013760bf794cb31630552e03f9518a
Abdul Rahim <zero_degree_96@hotmail.com>
2021-09-23 23:54:05 +0800

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,4 @@ alias config='/usr/bin/git --git-dir=/home/a_rahim/dotfiles/ --work-tree=/home/a
 export EDITOR=nvim
 export PATH="/home/a_rahim/.npm-global/bin:$PATH"
 export LC_ALL=en_US.UTF-8
+alias nv='nvim'
